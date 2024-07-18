9013c88ee682eb9d1ae96e5d7489a306993f9c3e
goot72 <157259472+goot72@users.noreply.github.com>
2024-07-18 15:02:18 -0700

    added info to learn more

--- client/src/components/learnMore.js
+++ client/src/components/learnMore.js
@@ -1,6 +1,7 @@
 // src/components/ButtonLink.js
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './learnMore.css';
 
 
 const ButtonLink = () => {
@@ -11,10 +12,26 @@ const ButtonLink = () => {
   };
 
   return (
-    <div>
-      <h1>Welcome to My Page</h1>
-      <p>This is a basic HTML structure in a React component.</p>
-      <button onClick={() => alert('Button clicked!')}>Click Me</button>
+    <div className="learn-more-page">
+      <h2>Learn More About Our Services</h2>
+      <div className='row'>
+      <div className='column'>
+        <h3>About FLYPATH</h3>
+        <p>FLYPATH's mission in to simplify the process of finding and booking plane tickets for travelers. Users can easily search for flights by entering key details such as their departure date, the number of passengers, destination, and preferred class of service. The application streamlines the search process, presenting users with available flight options that match their criteria, thereby enhancing the overall travel planning experience.</p>
+        </div>
+      <div className='column'>
+        <h3>Key Features</h3>
+        <li>User Input: Collects essential travel information such as departure date, number of passengers, destination, and travel class.</li>
+        <li>Flight Serach: Queries available flights based on user input and displays relevant options.</li>
+        <li>User-Friendly Interface: Simple and intuitive design to ensure ease of use.</li>
+        </div>
+        <div className='column'>
+          <h3>Goals</h3>
+          <li>Efficency: Reduce the time and effort required for users to find suitable flights.</li>
+          <li>Convinence: Provide a seamless and hassle-free experience for booking plane tickets.</li>
+          <li>Accessibility: Ensure application is easy to navigate for users of all technical skill levels.</li>
+        </div>
+    </div>
     </div>
   );
 };
